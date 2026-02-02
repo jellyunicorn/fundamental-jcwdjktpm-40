f053ea171a3528ab366832b639882b787047d742
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-02-02 14:25:58 +0700

    reorganisasi folder, file materi day 3

---
+++ materi/day3/day3.ts
@@ -0,0 +1,22 @@
+/* 
+    ========= [If-Else Statement] =========
+
+    if(condition) {
+        code block
+    } else if {
+        code block
+    }
+    ...
+    } else {
+      code block  
+    }
+
+*/
+
+const age: number = 17;
+
+if (age >= 17) {
+  console.log("Anda bisa buat KTP");
+} else {
+  console.log("blom");
+}
