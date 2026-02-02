a355fe9757bd9e2b7846db4779c017d4650e1efc
Keenan Satriatama <cvnvnasdf24@gmail.com>
2026-02-02 09:57:43 +0700

    feat: day2.js functionality

--- exercise/day2/day2.js
+++ exercise/day2/day2.js
@@ -1,5 +1,11 @@
 const prompt = require('prompt-sync')();
 
 let destinasi = prompt('Masukkan destinasi anda: ');
+let jarak = prompt('Masukkan jarak ke destinasi (km): ');
+let kecepatan = prompt('Masukkan kecepatan (km/h): ');
 
-console.log(destinasi);
+const waktu = jarak / kecepatan;
+
+console.log("Waktu perjalanan yang akan kamu tempuh ke destinasi " +
+    destinasi + " adalah " + waktu + " jam"
+);
